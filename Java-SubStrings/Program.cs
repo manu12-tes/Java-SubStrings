2a897c3f40243538ca5fc77d57cb19900ea5a696
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 02:46:19 -0500

    Agregar archivos de proyecto.

---
+++ Java-SubStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_SubStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame una cadena");
+            String cadena = Console.ReadLine();
+            Console.WriteLine("Dame posicion inicial");
+            int inicio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dame posicion final");
+            int final = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(cadena.Substring(inicio,final));
+
+        }
+    }
+}
